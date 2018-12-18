96916b9c7046a103e1edc1b8e1c1369386c25f51
Ivan Lucas <igoncalves@thecode.co>
2018-12-18 16:44:54 +1000

    Updating name value in Firebase and Fixing styles

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,37 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react';
+import './App.scss';
 
 // Firebase App is always required and must be first
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
 // Add additional services that you want to use
-import 'firebase/database'
-import 'firebase/functions'
+import 'firebase/database';
+import 'firebase/functions';
 
-class App extends Component {
-  constructor() {
-    super()
+export default class App extends React.Component {
+	state = {
+		name: ''
+	};
+	componentDidMount() {
+		const database = firebase.database();
 
-    this.state = {
-      name: ''
-    }
-  }
-  componentDidMount() {
-    const database = firebase.database();
+		let name = database.ref('name');
+		name.on('value', snapshot => {
+			this.setState({
+				name: snapshot.val()
+			});
+		});
+	}
+	handleInputChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+	handleFormSubmit = e => {
+		e.preventDefault();
+		const itemsRef = firebase.database().ref('name');
+		const item = this.state.name;
 
-    let name = database.ref('name');
-    name.on('value', snapshot => {
-      this.setState({
-        name: snapshot.val()
-      })
-    })
-  }
-  render() {
-    const { name } = this.state
-    return (
-      <div className="App">
-        <h1>{name ? `Hey, ${name}` : 'Loading...'}</h1>
-      </div>
-    );
-  }
+		// Set the new value in Firebase Realtime database
+		itemsRef.set(item);
+
+		// Reset the form after submitting
+		this.refs.form.reset();
+	};
+	render({ name } = this.state) {
+		return (
+			<React.Fragment>
+				<h1>{name ? `Hey, ${name}` : 'Loading...'}</h1>
+				<form onSubmit={this.handleFormSubmit} ref="form">
+					<input
+						type="text"
+						name="name"
+						onChange={this.handleInputChange}
+						ref="name"
+						placeholder="Put your new name here!!"
+					/>
+					<input type="submit" value="Submit!" />
+				</form>
+			</React.Fragment>
+		);
+	}
 }
-
-export default App
