aeaf2593af7f363eeccb36510ac6a8ca579e9388
Ivan Lucas <igoncalves@thecode.co>
2018-12-31 02:01:43 +1000

    Minor changes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,57 +1,59 @@
-import React from 'react';
-import './App.scss';
+import React from 'react'
+import './App.scss'
 
 // Firebase App is always required and must be first
-import firebase from 'firebase/app';
+import firebase from 'firebase/app'
 // Add additional services that you want to use
-import 'firebase/database';
-import 'firebase/functions';
+import 'firebase/database'
+import 'firebase/functions'
 
-export default class App extends React.Component {
+class App extends React.Component {
 	state = {
 		name: ''
-	};
+	}
 	componentDidMount() {
-		const database = firebase.database();
+		const database = firebase.database()
 
-		let name = database.ref('name');
+		let name = database.ref('name')
 		name.on('value', snapshot => {
 			this.setState({
 				name: snapshot.val()
-			});
-		});
+			})
+		})
 	}
 	handleInputChange = e => {
 		this.setState({
 			[e.target.name]: e.target.value
-		});
-	};
+		})
+	}
 	handleFormSubmit = e => {
-		e.preventDefault();
-		const itemsRef = firebase.database().ref('name');
-		const item = this.state.name;
+		e.preventDefault()
+		const itemsRef = firebase.database().ref('name')
+		const item = this.state.name
 
 		// Set the new value in Firebase Realtime database
-		itemsRef.set(item);
+		itemsRef.set(item)
 
 		// Reset the form after submitting
-		this.refs.form.reset();
-	};
+		this.refs.form.reset()
+	}
 	render({ name } = this.state) {
 		return (
 			<React.Fragment>
 				<h1>{name ? `Hey, ${name}` : 'Loading...'}</h1>
-				<form onSubmit={this.handleFormSubmit} ref="form">
+				<form onSubmit={this.handleFormSubmit} ref='form'>
 					<input
-						type="text"
-						name="name"
+						type='text'
+						name='name'
 						onChange={this.handleInputChange}
-						ref="name"
-						placeholder="Put your new name here!!"
+						ref='name'
+						placeholder='Put your new name here!!'
 					/>
-					<input type="submit" value="Submit!" />
+					<input type='submit' value='Submit!' />
 				</form>
 			</React.Fragment>
-		);
+		)
 	}
 }
+
+export default App
